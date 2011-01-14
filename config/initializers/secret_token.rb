2bafd7e2f0b85e0dc29235071eb0714701a077fc
rabarroso <ra.barroso@gmail.com>
2011-01-14 23:38:09 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tallerarmero::Application.config.secret_token = '0f2aab56acbc3c6b698b1d79e78f3c2bcba34d8409471a3390ed1a3c2a87fb73d72c464b0345607ffe36356badeb80500d8e8c2a6eaf1b19ab3c60040b4b799e'
