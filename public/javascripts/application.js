44642d36f84d80d4178af1d521d35ed12addfaf9
rabarroso <ra.barroso@gmail.com>
2011-01-17 23:49:14 +0100

    menu scrollable added

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,3 @@
 $(document).ready(function(ev){
-
-	$(window).scroll(function () {
-		if ($(window.pageYOffset)[0] > 708) {
-			$('div.about_right').css('position','fixed');
-			$('div.about_right').css('top','20px');
-		}else {
-			$('div.about_right').css('position','absolute');
-			$('div.about_right').css('top','0');
-		}
-	});
     
 });
