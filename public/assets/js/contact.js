5f41f275e76eff50ede4b5dcfc8c052236d42506
Raul Barroso <ra.barroso@gmail.com>
2014-08-16 18:40:05 -0700

    Reduce images size, import only necessary js libs

---
+++ public/assets/js/contact.js
@@ -0,0 +1,37 @@
+var map;
+var latitude = 40.4336599;
+var longitude = -3.6819766;
+var default_zoom = 17;
+
+$(document).ready(function(ev) {
+  initialize();
+});
+
+function initialize() {
+  var myLatlng = new google.maps.LatLng(latitude, longitude);
+  var myOptions = {
+      scrollwheel: false,
+      zoom: default_zoom,
+      center: myLatlng,
+      disableDefaultUI: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+
+  map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+  var contentString = '<div id="infoWindow">' + '<div id="siteNotice">' + '</div>' + '<h4 id="title_infowindow" class="title_infowindow">Armería Viaji</h4>' + '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: 'Armería Viaji'
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map, marker);
+  });
+}
